58f6c97406918b6fc1cbacd189e5e3655d36dd63
Jgpro13 <80963073+Jgpro13@users.noreply.github.com>
2021-06-21 21:56:27 -0300

    Add files via upload

---
+++ javascript/funcoes.js
@@ -0,0 +1,24 @@
+function mudaPaginaTrab1(){
+    window.location.href = "1opcao.html";
+}
+
+function mudaPaginaTrab2(){
+    window.location.href = "2opcao.html";
+}
+
+function mudaPaginaTrab3() {
+    window.location.href = "3opcao.html";
+}
+
+function mudaPaginaTrab4(){    
+    window.location.href = "4opcao.html";
+}
+
+function mudaPaginaTrab5(){    
+    window.location.href = "5opcao.html";
+}
+
+function voltaPagina1(){
+    window.location.href = "../index.html";
+}
+
